1a06127ad6951e213228027569fbb5ff09b9f1e6
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-10-31 13:50:12 +0200

    Adding send parameters

--- src/main/java/com/amazonaws/App.java
+++ src/main/java/com/amazonaws/App.java
@@ -116,11 +116,12 @@ public class App {
             //System.out.println("Result: " + putItemResult);
 
             // Add another item
-            item = newItem(KidName, KidTime);
-            putItemRequest = new PutItemRequest(tableName, item);
-            putItemResult = dynamoDB.putItem(putItemRequest);
-            System.out.println("Result: " + putItemResult);
-
+            if(KidName.length() != 0 && KidTime.length() != 0) {
+                item = newItem(KidName, KidTime);
+                putItemRequest = new PutItemRequest(tableName, item);
+                putItemResult = dynamoDB.putItem(putItemRequest);
+                System.out.println("Result: " + putItemResult);
+            }
             HashMap<String, Condition> scanFilter = new HashMap<String, Condition>();
             Condition condition = new Condition()
                     .withComparisonOperator(ComparisonOperator.EQ)
